52a7f2e61951dd6276375495a15acec0445e042c
Hynek Cígler <hynek.cigler@mail.muni.cz>
2020-04-18 15:44:20 +0200

    prevalence

--- polychoric/server.R
+++ polychoric/server.R
@@ -6,7 +6,7 @@ shinyServer(function(input, output, session) {
 # prepare -----------------------------------------------------------------
   
   # output$senzitivita <- renderText("125")
-  # output$specificita <- renderText("666")
+  output$specificita <- renderText("666")
   
   
   SENZ <- reactive({if(input$typ == "PCR") {
@@ -19,14 +19,6 @@ shinyServer(function(input, output, session) {
   
   prevalence <- reactive({
     input$prevalence/100
-  })
-  
-  populace <- reactive({
-    if (input$republika == T) {
-      10649800
-    } else {
-      input$populace
-    }
   })
   
   
@@ -42,35 +34,19 @@ shinyServer(function(input, output, session) {
   output$senzitivita <- renderText(SENZ())
   output$specificita <- renderText(SPEC())
   
-  trueSPEC <- reactive({
-    input$truespec
-  })
-  trueSENZ <- reactive({
-    input$truesenz
-  })
-  
   
 
   sick <- reactive({
-    populace() * prevalence()
+    input$populace * prevalence()
   })
   healthy <- reactive({
-    populace() * (1-prevalence())
+    input$populace * (1-prevalence())
   })
   TP <- reactive({
-    if(input$error == T) {
-      SENZ()*sick()
-    } else {
-      trueSENZ()*sick()
-    }
-
+    SENZ()*sick()
   })
   TN <- reactive({
-    if(input$error == T) {
-      SPEC()*healthy()
-    } else {
-      trueSPEC()*healthy()
-    }
+    SPEC()*healthy()
   })
   FN <- reactive({
     sick()-TP()
@@ -88,19 +64,19 @@ shinyServer(function(input, output, session) {
   
   output$TP <- renderText({
     paste0(formatC(TP(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC(TP()/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))
+           formatC(TP()/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))
   })
   output$FN <- renderText({
     paste0(formatC(FN(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC(FN()/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC(FN()/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   output$TN <- renderText({
     paste0(formatC(TN(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC(TN()/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC(TN()/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   output$FP <- renderText({
     paste0(formatC(FP(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC(FP()/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC(FP()/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   
 
@@ -109,106 +85,37 @@ shinyServer(function(input, output, session) {
 
   output$positive <- renderText({
     paste0(formatC(FP()+TP(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC((FP()+TP())/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC((FP()+TP())/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   
   output$negative <- renderText({
     paste0(formatC(FN()+TN(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC((FN()+TN())/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC((FN()+TN())/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   
   output$sick <- renderText({
     paste0(formatC(FN()+TP(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC((FN()+TP())/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC((FN()+TP())/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   
   output$healthy <- renderText({
     paste0(formatC(FP()+TN(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC((FP()+TN())/populace()*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
+           formatC((FP()+TN())/input$populace*100, digits=1, decimal.mark = ",", format="f"), ("%)"))  
   })
   
   output$total <- renderText({
     paste0(formatC(TP()+FP()+TN()+FN(), big.mark = ".", format = "d", decimal.mark = ","), " (", 
-           formatC((TP()+FP()+TN()+FN())/populace()*100, digits=0, decimal.mark = ",", format="f"), ("%)"))  
-  })
-  
-
-# * Prevalence v populaci -------------------------------------------------
-  
-  correction <- reactive({
-    (((FP()+TP())/populace()) + SPEC() - 1)/(SENZ() + SPEC() - 1)
-  })
-
-  output$positive2 <- renderText({
-    formatC((FP()+TP())/populace()*100, digits=1, decimal.mark = ",", format="f")
-  })
-  
-  output$bias1 <- renderText({
-    result <- ((FP()+TP())/populace() - prevalence())*100
-    result <- round(result, 8)
-    formatC(result, digits=2, decimal.mark = ",", format="f")
-  })
-  
-  output$positive_correct <- renderText({
-    result <- correction()*100
-    formatC(result, digits=1, decimal.mark = ",", format="f")
-  })
-  
-  output$positive_correct_bias1 <- renderText({
-    correction <- round(correction(), 8)
-    formatC((correction - prevalence())*100, digits=2, decimal.mark = ",", format="f")
-  })
-  
-  output$prevalence1 <- renderText({
-    input$prevalence
-  })
-  output$prevalence2 <- renderText({
-    input$prevalence
-  })
-
-  binom1 <- reactive({
-    prop.test(x = round(FP()+TP()), 
-               n = populace(), 
-               p = input$prevalence/100)
-  })
-  binom2 <- reactive({
-    prop.test(x = round(populace() * correction(), 0), 
-               n = populace(), 
-               p = input$prevalence/100)
-  })
-  
-  output$ci <- renderText({
-    paste(round(binom1()$conf.int[1]*100, 1), round(binom1()$conf.int[2]*100, 1), sep="; ")
-  })
-  
-  output$ci_correct <- renderText({
-    if(correction() < 0) {
-      "CI nelze spočítat, protože korekce poskytuje záporné hodnoty prevalence"
-    } else {
-      paste(round(binom2()$conf.int[1]*100, 1), round(binom2()$conf.int[2]*100, 1), sep="; ")
-    }
-    
-  })
-  
-  output$p <- renderText({
-    round(binom1()$p.value, 3)  })
-  output$p_correct <- renderText({
-    if(correction() < 0) {
-      "p-hodnotu nelze spočítat, protože korekce poskytuje záporné hodnoty prevalence"
-    } else {
-      round(binom2()$p.value, 3)
-    }
-    
+           formatC((TP()+FP()+TN()+FN())/input$populace*100, digits=0, decimal.mark = ",", format="f"), ("%)"))  
   })
   
 
 # * přesnost --------------------------------------------------------------
 
   output$spolehlivost <- renderText({
-    formatC((TP()+TN())/populace()*100, digits=1, decimal.mark = ",", format="f")
+    formatC((TP()+TN())/input$populace*100, digits=1, decimal.mark = ",", format="f")
   })  
   output$nespolehlivost <- renderText({
-    formatC((FP()+FN())/populace()*100, digits=1, decimal.mark = ",", format="f")
+    formatC((FP()+FN())/input$populace*100, digits=1, decimal.mark = ",", format="f")
   }) 
   output$PP <- renderText({
     formatC(TP()/(TP()+FP())*100, digits=1, decimal.mark = ",", format="f")
@@ -217,9 +124,6 @@ shinyServer(function(input, output, session) {
     formatC(TN()/(TN()+FN())*100, digits=1, decimal.mark = ",", format="f")
   }) 
   
-
-# grafy -------------------------------------------------------------------
-
   output$plot1 <- renderPlot({
     cols <- c("firebrick2", "lightgreen")
     x <- cbind("pozitivní" = c(FP(), TP())/(TP() + FP()),
@@ -228,7 +132,7 @@ shinyServer(function(input, output, session) {
                      ylab="výsledek testu", xlab = "chybný výsledek (%)", 
                      cex.names = 1.5, cex.axis = 1.3, cex.lab = 1.6, cex.main = 2,
                      col = cols, 
-                     main = "Podle výsledku testu")
+                     main = "Podíl chybných výsledků (podle výsledku testu)")
     text(1, mybar, paste0(round(c(x[1,1], x[1,2]), 2), " %"), cex=1.5, pos=4)
     legend("top", c("chybně", "správně"), horiz = T, col = cols, pch=15, pt.cex=2, 
            title="Identifikováno:")
@@ -242,7 +146,7 @@ shinyServer(function(input, output, session) {
                      ylab="ve skutečnosti", xlab = "chybný výsledek (%)", 
                      cex.names = 1.5, cex.axis = 1.3, cex.lab = 1.6, cex.main = 2,
                      col = cols, 
-                     main = "Podle zdravotního stavu")
+                     main = "Pravděpodobnost chyby (podle zdravotního stavu)")
     text(1, mybar, paste0(round(c(x[1,1], x[1,2]), 2), " %"), cex=1.5, pos=4)
     legend("top", c("chybně", "správně"), horiz = T, col = cols, pch=15, pt.cex=2, 
            title="Identifikováno:")
--- polychoric/ui.R
+++ polychoric/ui.R
@@ -3,66 +3,21 @@ library(shiny)
 year <- substr(Sys.Date(), 1, 4)
 
 shinyUI(fluidPage(
-  titlePanel("COVID-19: Chybovost a spolehlivost testů"),
+  titlePanel("Polychoric Correlation: Bias simulation"),
   tags$head(includeHTML(("ga.html"))),
-  title = "COVID-19: Chybovost a spolehlivost testů",
+  title = "Polychoric Correlation: Bias simulationů",
   hr(),
 
 
-fluidRow(  column(
-  h4("Prevalence viru v populaci"),
-  sliderInput("prevalence", label = "Kolik procent lidí je skutečně nakažených?", min = 0, max = 100, value = 5, step = .1, 
-              animate=animationOptions(interval = 10, loop = T), 
-              post=" %"),
-  checkboxInput("republika", label = "Chcete použít namísto vzorku počet obyvatel České republiky?", 
-                value = F), 
-  conditionalPanel(
-    condition ="input.republika == 0",
-    sliderInput("populace", label = "Velikost vzorku", ticks = F,
-                min = 100, max = 100000, value = 10000, step = 10)
-  ),
-  conditionalPanel(
-    condition ="input.republika == 1",
-    p(tags$small(em("Česká republika má asi 10.649.800 obyvatel."))),
-  ),
-
-  
-  hr(),
-  h4("Parametry použitého testu"),
-  radioButtons("typ", label = "Který test používáte?", 
-               choices = list(PCR="PCR", 
-                              rychlotest="rychlo", 
-                              vlastní="user")),
-  p(tags$strong("PCR:"), " senzitivita 0,937; specificita 0,999", br(),
-    tags$strong("rychlotest:"), ": senzitivita 0,7; specificita 0,8"),
-  checkboxInput("error", "Jsou parametry testu správné?", value = T),
-  conditionalPanel(
-    condition = "input.typ == 'user'",
-    h4("Odhad parametrů testu:"),
-    p(tags$small("Se kterými pracujeme v odhadech.")),
-    sliderInput("senz", label = "senzitivita:", min = .5, max = 1, value = .7, step = .01, 
-                animate=animationOptions(interval = 200, loop = T)), 
-    sliderInput("spec", label = "specificita:", min = .5, max = 1, value = .8, step = .01, 
-                animate=animationOptions(interval = 200, loop = T))),
-  conditionalPanel(
-    condition = "input.error == 0", 
-    h4("Skutečné parametry testu:"),
-    p(tags$small("Které test má ve skutečnosti.")),
-    sliderInput("truesenz", label = "senzitivita", min = .5, max = 1, value = .7, step = .01,
-                animate = animationOptions(interval = 200, loop = T)),
-    sliderInput("truespec", label = "specificita", min = .5, max = 1, value = .8, step = .01,
-                animate = animationOptions(interval = 200, loop = T))
-  ),
-
-  
-  
-  hr(),
+fluidRow(  
+  column(
+    numericInput("N", "Sample size", value = 200, min = 10),
+    hr(),
   
   
   # * copyright -------------------------------------------------------------
   
-  HTML("<p>Tento jednoduchý nástroj byl vyvinut k ilustraci vlastností diagnostických testů, 
-    používaných (nejen) během epidemie viru COVID-19.</p><p>&copy; ", 
+  HTML("<p>This application serves for simulation bias in polychoric</p><p>&copy; ", 
        year, 
        " Hynek Cígler, Ph.D.<br>
          Katedra psychologie, Fakulta sociálních studií<br>
@@ -113,50 +68,31 @@ column(h3("Výsledek testu vs. skutečnost"),
        hr(),
        
        
-       # * přesnost a odhad populace -----------------------------------------------------------------
-
+       # * plots -----------------------------------------------------------------
+       
        h3("Přesnost testu"),
        p("Spolehlivost testu:", textOutput("spolehlivost", inline = T), "%", br(), 
          "Nespolehlivost testu:", textOutput("nespolehlivost", inline = T), "%", br(), 
          "Pozitivní prediktivní hodnota testu:", textOutput("PP", inline = T), "%", br(), 
-         "Negativní prediktivní hodnota testu:", textOutput("NP", inline = T), "%",
-         style = "font-size: 120%;"),
-       h3("Odhad prevalence"),
-       p("Pozorovaná prevalence je:", textOutput("positive2", inline = T), 
-         "% s 95% intervalem spolehlivosti CI", tags$sub("95%"), " = [", 
-         textOutput("ci", inline = T), "].", br(),
-         "Chyba oproti skutečné prevalenci ", textOutput("prevalence1", inline = T), 
-         "% je tedy", textOutput("bias1", inline = T),  "%, ", tags$em("p"), " = ", 
-         textOutput("p", inline = T), ".",
-         style = "font-size: 120%;"),
-       p("Korigovaná prevalence je:", textOutput("positive_correct", inline = T), 
-         "% s 95% intervalem spolehlivosti CI", tags$sub("95%"), " = [", 
-         textOutput("ci_correct", inline = T), "].", br(),
-         "Chyba oproti skutečné prevalenci ", textOutput("prevalence2", inline = T), 
-         "% je tedy", textOutput("positive_correct_bias1", inline = T),  "%, ", tags$em("p"), "=", 
-         textOutput("p_correct", inline = T), ".", br(), 
-         tags$small("Korigovaná prevalence by měla být očištěna o chybu odhadu způsobenou nepřesností testu."),
-         style = "font-size: 120%;"),
-       
-       
-       # * plots -----------------------------------------------------------------
-       
+         "Negativní prediktivní hodnota testu:", textOutput("NP", inline = T), "%", br(), 
+         style = "font-size: 120%;"
+       ),
        hr(),
-       
-       fluidRow(column(
-         h3("Podíl chybných výsledků"),
-         p("Jaká je pravděpodobnost, že vyšetřený člověk s pozitivním (resp. negativním) výsledkem testu 
+       h3("Podíl chybných výsledků"),
+       p("Jaká je pravděpodobnost, že vyšetřený člověk s pozitivním (resp. negativním) výsledkem testu 
               je ve skutečnosti nemocný (resp. zdravý)? Jinými slovy podíl lidí s určitým výsledkem testu, 
               u kterých je tento výsledek chybný. Výsledkem je vlastně jen prediktivní hodnota testu odečtená od jedné."),
-         plotOutput("plot1", width = "100%"), 
-         width = 6),
-         column(
-           h3("Pravděpodobnost chyby"),
-           p("Jaká je pravděpodobnost, že u zdravého (resp. nemocného) člověka bude výsledek testu chybný? 
+       plotOutput("plot1", width = "800"),
+       hr(),
+       h3("Pravděpodobnost chyby"),
+       p("Jaká je pravděpodobnost, že u zdravého (resp. nemocného) člověka bude výsledek testu chybný? 
               Výsledkem je vlastně jen senzitivita, resp. specificita testu odečtená od jedné."),
-           plotOutput("plot2", width = "100%"), 
-           width = 6)), 
+       plotOutput("plot2", width = "800"),
+       
+      
+       
        br(),
+       
        width = 9
        
 )), fluidRow(
@@ -228,16 +164,6 @@ column(h3("Výsledek testu vs. skutečnost"),
       tags$li(strong("Negativní prediktivní hodnota"), "podíl zdravých osob mezi lidmi, 
           které identifikuje jako zdravé. Lze spočítat jako TN/(TN+FN)."),
     ),
-    h4("Odhad skutečné prevalence a nepřesnost diagnostiky"),
-    p("Nepřesnost diagnostického nástroje má vliv na odhad promořenosti populace. 
-      Zjednodušeně lze říct, že čím větší je chybovost testu, tím blíže je odhadovaná prevalence 
-      blízká 50 %. Tuto nepřesnost však lze možné korigovat podle vzorce EST = (PREV+SENZ-1)/(SENZ + SPEC -1 
-      kde PREV je pozorovaná prevalence, SENZ a SPEC senzitivita a specificita testu a EST je 
-      odhadovaná skutečná prevalence po korekci.", br(), 
-      "Pro účely této korekce je nicméně nezbytně nutné znát skutečnou senzitivitu a specificitu. 
-      Aplikace umožňuje nastavit jinou specificitu a senzitivitu (stačí zrušit zakliknutí 
-      Jsou parametry testu správné)."),
-    
     p("Je důležité si uvědomit, že prevalence nemoci záleží na konkrétní populaci. Jiná bude 
   u náhodně vybraného občana České republiky, jiná (a výrazně vyšší) bude u člověka, který vykazuje 
   běžné symptomy onemocnění virem COVID-19."),
